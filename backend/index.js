f56a8128b6f06b753ee78894ebf46810a25ea83f
Tejash <tejashmaru2003@gmail.com>
2025-05-31 22:45:29 +0530

    Connection with mongo DB Atlas

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+require('./models/db'); // Ensure this file exists and is correctly set up to connect to MongoDB
 
 const PORT = process.env.PORT || 8080;
 
--- backend/models/db.js
+++ backend/models/db.js
@@ -1,1 +1,12 @@
 const mongoose = require('mongoose');
+
+
+const mongo_url = process.env.MONGO_CONN;
+
+mongoose.connect(mongo_url)
+  .then(() => {
+    console.log("MongoDB connected successfully");
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+  });
