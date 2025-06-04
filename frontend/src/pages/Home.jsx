7bb08bc4a66ae7411ec42242b8f008b7edf4d3b2
Tejash <tejashmaru2003@gmail.com>
2025-06-04 19:24:52 +0530

    logged in user info only in home component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { handleSuccess } from '../utils';
+import { ToastContainer } from 'react-toastify';
 
 const Home = () => {
+  const [loggedInUser, setLoggedInUser] = useState('');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setLoggedInUser(localStorage.getItem('loggedInUser'))
+  },[])
+
+  const handleLogout = (e) => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('loggedInUser');
+    handleSuccess("User Loggedout Successfully");
+    setLoggedInUser('');
+    setTimeout(() => {
+      navigate("/login");
+    },1000)
+  }
   return (
     <div>
-      <h1 className='bg-blue-600'>Welcome to the Home Page</h1>
+      <h1 className='bg-blue-600'>{loggedInUser}</h1>
+      <button onClick={handleLogout}>Logout</button>
+
+      <ToastContainer />
     </div>
   )
 }
