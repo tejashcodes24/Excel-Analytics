1268eec1778e1efa6cf33541366106de8344f98a
Tejash <tejashmaru2003@gmail.com>
2025-06-18 18:00:39 +0530

    changed routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import { Navigate, Routes } from 'react-router-dom'
-//import './App.css'
+import './App.css'
 import Login from './pages/Login'
 import { Route } from 'react-router-dom'
 import SignUp from './pages/SignUp'
 import Home from './pages/Home'
 import RefreshHandler from './refreshHandler'
+import LandingPage from './pages/LandingPage'
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -13,15 +14,16 @@ function App() {
     return isAuthenticated ? element : <Navigate to="/login" />;
   }
   return (
-    <div className='App'>
-      <RefreshHandler setIsAuthenticated={setIsAuthenticated} />  
-      <Routes>
-        <Route path="/" element={<Navigate to = "/login" />} />
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/signup" element={<SignUp/>}/>
-        <Route path="/home" element={<PrivateRoute element={<Home/>} />}/>
-      </Routes>
-    </div>
+      <div className="App">
+        <RefreshHandler setIsAuthenticated={setIsAuthenticated} />  
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/landing" element={<LandingPage />} />
+          <Route path="/home" element={<PrivateRoute element={<Home />} />} />
+        </Routes>
+      </div>
   )
 }
 
